563c6a1940f88bc25e3c4bec479077a47a48371e
Daniel Granada <j.daniel_granada@hotmail.com>
2023-04-12 06:58:58 -0500

    Was added a timer funtion nad a second screen (is not working yet)

--- game/game_screen.py
+++ game/game_screen.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 def clean_screen():
     os.system('cls' if os.name == 'nt' else 'clear')                        # Clean the screen
@@ -6,8 +7,11 @@ def clean_screen():
 def firts_screen():
 
     clean_screen()
+    yes_no_choice = ''
 
-    print(
+    while yes_no_choice not in ['1','2']:
+
+        print(
 '''
 Welcome to Rock, Paper, Scissors game!
 
@@ -15,16 +19,52 @@ Welcome to Rock, Paper, Scissors game!
     1> Yes.
     2> No.
 '''
+        )
+
+        yes_no_choice = int(input('Pleas you must to choose the options (1 or 2): '))
+
+        if yes_no_choice == 1:
+
+            charging_screen()
+            clean_screen()
+            print('you are playing')
+        
+        elif yes_no_choice == 2:
+
+            clean_screen()
+            print('that has been all, thank you')
+            break
+
+def second_screen():
+
+    n_round = 1
+    p_score = 0
+    ai_score = 0
+    
+    clean_screen()
+
+    print(f'''
+    Your are playing!
+
+Round{n_round} | Player score => {p_score} - AI score => {ai_score}
+
+    choose between:
+
+    1> Rock.
+    2> Paper.
+    3> Sicssors.
+    4> Exit.    
+'''
     )
 
-    yes_no_choice = int(input('Pleas ou must to choose the options (1 or 2): '))
+def charging_screen():
 
-    if yes_no_choice == 1:
+    counter = 0.5
+    message = '.....'
 
+    for i in range (0,3):
         clean_screen()
-        print('you are playing')
-    
-    elif yes_no_choice == 2:
-
-        clean_screen()
-        print('that has been all, thank you')
+        print(f'charging {message}')
+        time.sleep(counter)
+        counter += 0.5
+        message = message * 2
