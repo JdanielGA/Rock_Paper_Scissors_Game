4f8268f7c70cc31dc2d93ee9a3183f4b4429f2b7
Daniel Granada <j.daniel_granada@hotmail.com>
2023-07-22 15:42:54 -0500

    README file added, score mechanics added and error handling to handle invalid inputs and display appropriate error messages added

--- game/game_screen.py
+++ game/game_screen.py
@@ -8,6 +8,7 @@ def firts_screen():
 
     while yes_no_choice not in ['1','2']:
 
+        tools.clean_screen()
         print(
 '''
 Welcome to Rock, Paper, Scissors game!
@@ -21,33 +22,33 @@ Welcome to Rock, Paper, Scissors game!
         yes_no_choice = int(input('Pleas you must to choose the options (1 or 2): '))
 
         if yes_no_choice == 1:
-
             tools.charging_screen()
             tools.clean_screen()
+            t_rounds = int(input('Introduce the number of rounds that you want to play: '))
             print('\n   You are playing!')
             tools.time.sleep(2)
-            second_screen()
+            second_screen(t_rounds)
         
         elif yes_no_choice == 2:
-
             tools.clean_screen()
-            print('that has been all, thank you')
-            tools.time.sleep(2)
-            tools.clean_screen()
+            end_screen()
             break
 
-def second_screen():
+def second_screen(t_rounds):
 
+    tools.clean_screen()
+    t_rounds = t_rounds
     n_round = 1
     p_score = 0
     ai_score = 0
-    
-    tools.clean_screen()
 
-    print(f'''
+    while True:
+
+        tools.clean_screen()
+        print(f'''
     Your are playing!
 
-Round{n_round} | Player score => {p_score} - AI score => {ai_score}
+Round {n_round} of {t_rounds} | Player score => {p_score} - AI score => {ai_score}
 
     choose between:
 
@@ -57,5 +58,33 @@ Round{n_round} | Player score => {p_score} - AI score => {ai_score}
     4> Exit.    
 '''
     )
+        user_input = input('Type the number of the chosen option: ')
 
-    mechanics.game_mechanics()
+        try:
+            user_choice = int(user_input)
+
+            if user_choice == 4:
+                end_screen()
+                break
+
+            elif user_choice in [1, 2, 3]:
+                p_score, ai_score, = mechanics.game_mechanics(user_choice, p_score, ai_score)
+                n_round += 1
+
+            else:
+                print("Invalid input! Please choose a valid option.")
+                tools.time.sleep(2)
+                tools.clean_screen()
+        
+        except ValueError:
+            print("Invalid input! Please enter a valid number.")
+            tools.time.sleep(2)
+            tools.clean_screen()
+    
+
+def end_screen():
+
+    tools.clean_screen()
+    print('that has been all, thank you.')
+    tools.time.sleep(2)
+    tools.clean_screen()
--- game/mechanics.py
+++ game/mechanics.py
@@ -1,29 +1,30 @@
 import tools
 
-def game_mechanics():
+def game_mechanics(user_choice, p_score, ai_score):
 
     options = ['Rock','Paper','Scissors']
-
-    user_choice = int(input('Type option the number of the chosen option: '))
+    user_choice = user_choice
     chosen_option = options[user_choice-1]
     ai_player = tools.ai_player()
     ai_choice = options[ai_player]
 
     print(f'\n you have chosen {chosen_option} and the ai has chosen {ai_choice}')
-    result = match(chosen_option,ai_choice)
-    print(result)
-
-    tools.time.sleep(3)
+    result = match(chosen_option, ai_choice, p_score, ai_score)
+    print(f'\n {result[0]}')
+    tools.time.sleep(2)
     tools.clean_screen()
+    return result[1], result[2]
 
 
-def match(chosen_option, ai_choice):
+def match(chosen_option, ai_choice, p_score, ai_score):
 
     if chosen_option == ai_choice:
-        return 'Tie'
+        return 'Tie', p_score, ai_score
     
     elif (chosen_option == 'Rock' and ai_choice == 'Scissors') or  (chosen_option == 'Paper' and ai_choice == 'Rock') or (chosen_option == 'Scissors' and ai_choice == 'Paper'):
-        return 'You win'
+        p_score += 1
+        return 'You win', p_score, ai_score
     
     else:
-        return 'You lost'
+        ai_score += 1
+        return 'You lost', p_score, ai_score
